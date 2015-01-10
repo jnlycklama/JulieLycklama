6bf37655e80878bf3d06b2357675d7fe7570ae34
Julie Lycklama <jnlycklama@users.noreply.github.com>
2015-01-09 22:53:30 -0500

    no message

---
+++ resume.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Julie Lycklama</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="shortcut icon" href="https://40.media.tumblr.com/3bd2e927080954d9516ccdc325cd03">
+	<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.1.min.js"></script>
+	<script src="main.js"></script>
+</head>
+
+<body>
+	<div id="header">
+		<img src="Julie Lycklama.png" alt="header" />
+	</div>
+
+
+	<div id="navWrap">
+		<div id="nav">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="school.php">School</a></li>
+				<li><a href="projects.php">Projects</a></li>
+				<li><a href="about.php">About</a></li>
+				<li><a href="resume.php">Resume</a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="section">
+		<h1>Resume</h1>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+		<p>Filler text :)</p>
+
+	</div>
+</body>
+
+</html>
---
+++ school.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Julie Lycklama</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="shortcut icon" href="https://40.media.tumblr.com/3bd2e927080954d9516ccdc325cd03">
+	<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.1.min.js"></script>
+	<script src="main.js"></script>
+	<link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Bentham' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+	<div id="header">
+		<img src="Julie Lycklama.png" alt="header" />
+	</div>
+
+
+	<div id="navWrap">
+		<div id="nav">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="school.php">School</a></li>
+				<li><a href="projects.php">Projects</a></li>
+				<li><a href="about.php">About</a></li>
+				<li><a href="resume.php">Resume</a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="container">
+	<img id="back_img" src="computingJacket.jpg" alt="computing jacket" />
+	<div class="info">
+			
+			<h1>Queen's <span id="caps">University</span></h1>
+			<h2>Bachelor of Computing, Specialization in Software Design</h2>
+			<h2>Cumulative GPA: 3.54</h2>
+
+			<a href="#first"><span class="button">First Year</span></a><br><br>
+			<a href="#second"><span class="button">Second Year</span></a>
+		</div>
+	</div>
+	<div class="section_two">
+
+		<div id="first">
+			<br>
+			<div class="head">
+			<h1>First Year</h1>
+			<h2>First Term</h2>
+		</div>
+		<div class="content">
+			<div class="hold">
+				<h3>Introduction to Computing Science I</h3>
+				<p>Language: Python</p>
+				<p>Introduction to design and analysis of algorithms. Recursion, backtracking, and exits. Sequences, linked lists and references. Binary search trees. Elementary searching and sorting. Assertions and loop invariants. Order-of-magnitude complexity. Numerical computation. Documentation, testing and debugging.</p>
+				<p>Final Grade: A</p>
+				<br>
+				<h3>Women, Gender, Difference</h3>
+				<p>This course explores women, gender, and difference from feminist and anti-racist perspectives. It identifies the ways in which women's activism, politics, and experiences intersect with other gendered identifications such as race, location, class, (dis)ability, and sexuality. Lessons and texts will introduce feminism, the body, colonialism, gender performance, and strategies of resistance.</p>
+				<p>Final Grade: A</p>
+			</div>
+		</div>
+			<div class="head">
+			<h4>Term GPA: 4.00</h4>
+			<br>
+			<h2>Second Term</h2>
+		</div>
+			<div class="content">
+				<div class="hold">
+			<h3>Introduction to Computing Science II</h3>
+			<p>Language: Java</p>
+			<p>Introduction to object-oriented design, architecture, and programming. Use of packages, class libraries, and interfaces. Encapsulation and representational abstraction. Inheritance. Polymorphic programming. Exception handling. Iterators. Introduction to a class design notation. Applications in various areas.</p>
+			<p>Final Grade: A-</p>
+			<br>
+			<h3>Linear Algebra - Full Year</h3>
+			<p>An introduction to matrices and linear algebra. Emphasis on applications to biological and economic systems and to computer applications. Topics covered will include systems of equations, eigenvalues, recursions, orthogonality, regression analysis, and geometric transformations.</p>
+			<p>Final Grade: A+</p>
+			<br>
+			<h3>Differential and Integral Calculus - Full Year</h3>
+			<p>Differentiation and integration with applications to biology, physics, chemistry, economics, and social sciences; differential equations; multivariable differential calculus.</p>
+			<p>Final Grade: A-</p>
+			<br>
+			<h3>Introduction to Statistics</h3>
+			<p>A basic course in statistical methods with the necessary probability included. Topics include probability models, random variables, distributions, estimation, hypothesis testing, elementary nonparametric methods.</p>
+			<p>Final Grade: C-</p>
+			<br>
+			<h3>Principles of Psychology - Full Year</h3>
+			<p>An introductory survey of basic areas of psychology including perception, cognition, learning and motivation and their biological substrata. Also reviewed are child development, individual differences, social psychology and abnormal psychology. Research participation experience is provided for students on an individual voluntary basis. Students are encouraged to participate in up to five hours of research experimentation.</p>
+			<p>Final Grade: B</p>
+		</div>
+		</div>
+		<div class="head">
+			<h4>Term GPA: 3.43</h4>
+			<br>
+			<h3>Cumulative GPA: 3.54</h3>
+		</div>
+
+		</div>
+
+		<div id="second">
+			<div class="head">
+			<h1>Second Year</h1>
+			<h2>First Term</h2>
+		</div>
+		<div class="content">
+			<div class="hold">
+			<h3>Discrete Math for Computing Science</h3>
+			<p>Introduction to mathematical discourse and proof methods. Sets, functions, sequences, and relations. Properties of the integers. Introduction to graph theory. Introduction to combinatorics.</p>
+			<p>Final Grade: A-</p>
+			<br>
+			<h3>System Level Programming</h3>
+			<p>Language: C, bash</p>
+			<p>Basic concepts of Unix-like systems. Shells and scripting. System-level programming in the C language. Software development tools and techniques.</p>
+			<p>Final Grade: ?</p>
+			<br>
+			<h3>Web and Mobile Applications</h3>
+			<p>Language: HTML, CSS, Javascript, PHP, SQL</p>
+			<p>This course surveys current best practices for implementing attractive, usable, secure and maintainable web and mobile applications. Other issues considered include: accessibility, platform and browser independence, licensing of intellectual property, scalability, user privacy, and using web technologies in mobile development.</p>
+			<p>Final Grade: ?</p>
+			<br>
+			<h3>Business Fundamentals</h3>
+			<p>This course will develop an appreciation of the role of the manager and business in society. It will discuss Canadian and global trends and issues - political, economic, technological, and ethical - that affect business and management. The course will introduce the student to the nature of the modern corporate enterprise, large and small, and begin the building of business analysis skills.</p>
+			<p>Final Grade: ?</p>
+			<br>
+			<h3>Media and Popular Culture</h3>
+			<p>Course surveys a variety of popular media forms and genres (film, TV, radio, music, novels, magazines, advertising, news, Internet). Introduces contemporary cultural studies analysis concerning the impact of everyday media use on the formation of identities, perceptions, lifestyles and communities.</p>
+			<p>Final Grade: ?</p>
+		</div>
+		</div>
+		<div class="head">
+			<br>
+			<h4>Term GPA: ?</h4>
+			<br>
+			<h2>Second Term</h2>
+		</div>
+		<div class="content">
+			<div class="hold">
+			<h3>Logic for Computing Science</h3>
+			<p>Elements of mathematical logic with computing applications. Formal proof systems for propositional and predicate logic. Interpretations, validity, and satisfiability. Introduction to soundness, completeness and decidability.</p>
+			<p>Final Grade: ?</p>
+			<br>
+			<h3>Computer Architecture</h3>
+			<p>Language: Assembly</p>
+			<p>The descriptive levels of computer architecture. Instruction-set architectures. Assembly Language. Data representation. Support for operating-system management and high-level languages. Input/output and interrupts. Designing for performance. Digital Logic.</p>
+			<p>Final Grade: ?</p>
+			<br>
+			<h3>Software Specifications</h3>
+			<p>Language: ?</p>
+			<p>Introduction to techniques for specifying the behaviour of software, with applications of these techniques to design, verification and construction of software. Logic-based techniques such as loop invariants and class invariants. Automata and grammar-based techniques, with applications to scanners, parsers, user-interface dialogs and embedded systems. Computability issues in software specifications.</p>
+			<p>Final Grade: ?</p>
+			<br>
+			<h3>Data Structures</h3>
+			<p>Language: ?</p>
+			<p>Design and implementation of advanced data structures and related algorithms, including correctness and complexity analysis. </p>
+			<p>Final Grade: ?</p>
+			<br>
+			<h3>Programming Paradigms</h3>
+			<p>Language: LISP, Haskell, PROLOG</p>
+			<p>Review of imperative programming features. Introduction to other widely used programming paradigms. Functional programming languages, such as LISP and Haskell. Higher order functions, lazy evaluation, abstract and recursive types, structural induction, symbolic expressions. Logic programming languages, such as PROLOG. Operational interpretation of predicates and terms, proof search, unification, backtracking. Typical applications.</p>
+			<p>Final Grade: ?</p>
+		</div>
+		</div>
+		<div class="head">
+			<br>
+			<h4>Term GPA: ?</h4>
+			<br>
+			<h3>Cumulative GPA: 3.54</h3>
+		</div>
+
+
+		</div>
+
+		
+
+	</div>
+</body>
+
+</html>
